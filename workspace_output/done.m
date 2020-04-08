bf8e246cd79dd510e8b15af553242a6c53b1edf4
Johannes Rebling <johannesrebling@gmail.com>
2020-04-08 09:11:50 +0200

    add option to supress output of Done

--- workspace_output/done.m
+++ workspace_output/done.m
@@ -1,7 +1,14 @@
-function [] = done(tocHandle)
-  if nargin == 0
-    fprintf('done.\n');
-  else
-    fprintf('completed in %3.2fs.\n',tocHandle);
+function [] = done(tocHandle,showOutput)
+  % showOutput - optional, usefull in combination with verbose flag...
+  if nargin < 2
+    showOutput = 1; 
+  end
+  if showOutput
+    if nargin == 0
+      fprintf('done.\n');
+    else
+      fprintf('completed in %3.2fs.\n',tocHandle);
+    end
   end
 end
+
