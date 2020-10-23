7af713e0271f6754969fddc97f917da1c38627cb
Johannes Rebling <johannesrebling@gmail.com>
2020-10-23 18:31:01 +0200

    make find near great again

--- general/find_nearest.m
+++ general/find_nearest.m
@@ -1,34 +1,6 @@
-function [nearestVal,nearestIdx] = find_nearest(findVals,data,doWarn)
-  if nargin < 3
-    doWarn = true;
-  end
-  if doWarn
-    short_warn('Use dsearchn(data,findVals) is much more efficient!');
-  end
-  % [nearestVal,nearestIdx] = find_nearest(findVals,data)
-  % looks for nearerst val and index for findVals on data
-  findVals = double(findVals);
-  data = double(data);
-  % check for data comformity
-  if ~isvector(data)
-    error('ndims(data) > 2! data must be a vector!')
-  end
-  if ~isvector(findVals)
-    error('ndims(findVals) > 2! findVals must be a scalar or a vector!')
-  end
-
-  % handle scalar and vector inputs for findVals
-  if numel(findVals) > 1
-    nearestIdx = zeros(numel(findVals),1);
-    nearestVal = zeros(numel(findVals),1);
-    for iVal = 1:numel(findVals)
-      %FIXME is there a way to vectorize this?
-      [~,nearestIdx(iVal)] = min(abs(data-findVals(iVal)));
-      nearestVal(iVal) = data(nearestIdx(iVal));
-    end
-  else % return scalar answer
-    difference = abs(data-findVals);
-    [~,nearestIdx] = min(difference); %index of closest value
-    nearestVal = data(nearestIdx); %closest value
-  end
+function [nearestVal,nearestIdx,distance] = find_nearest(findVals,data)
+  findVals = single(findVals(:));
+  data = single(data(:));
+  [nearestIdx,distance] = dsearchn(data,findVals);
+  nearestVal = data(nearestIdx);
 end
