fa43ebf44e883e1f2b5f91c213bb7a55046550f2
Joe <johannesrebling@gmail.com>
2020-10-29 17:27:16 +0100

    gui util improvements / bug fixes

--- general/wrap_outlier_data.m
+++ general/wrap_outlier_data.m
@@ -2,9 +2,9 @@ function [data, lowLim, upLim] = wrap_outlier_data(data,percentiles)
   % [data] = wrap_outlier_data(data,[lowLim upLim]);
   % lowLim & upLim expressed as percentiles
   % i.e. percent of min max values to wrap
-
-  lowLim = prctile(data,percentiles(1)); 
-  upLim = prctile(data,percentiles(2)); 
-  data(data>upLim) = upLim;
-  data(data<lowLim) = lowLim;
+  
+  lowLim = prctile(data(:),percentiles(1)); 
+  upLim = prctile(data(:),percentiles(2)); 
+  data(data(:)>upLim) = upLim;
+  data(data(:)<lowLim) = lowLim;
 end
